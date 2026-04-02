6587345829650d35ee98af008aa7caac0d019069
ivance <rivance.0@gmail.com>
2026-04-02 19:39:44 +0800

    test: update spec file to match the new core

--- src/test/scala/cubical/TypeCheckSpec.scala
+++ src/test/scala/cubical/TypeCheckSpec.scala
@@ -1,41 +1,99 @@
 package cubical
 
+import org.scalatest.ParallelTestExecution
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+
 import java.io.File
 import scala.io.Source
+import scala.util.Using
 
-class TypeCheckSpec extends AnyFlatSpec with Matchers {
-  
-  val examplesDir = new File("examples")
-  val cttFiles = examplesDir.listFiles().filter(_.getName.endsWith(".ctt"))
-  
-  cttFiles.foreach { file =>
-    s"Type checker" should s"successfully check ${file.getName}" in {
-      val source = Source.fromFile(file)
-      val content = try source.mkString finally source.close()
-      
-      // Parse
-      val parseResult = Parser.parseModule(content)
-      parseResult match {
-        case Left(err) => fail(s"Parse error: $err")
-        case Right(decls) =>
-          // Convert to mutual decls format
-          val mutualDecls = decls.map {
-            case Decl.DefDecl(name, ty, body) => (name, (ty, body))
-            case Decl.DataDecl(name, _, ty, _) => (name, (ty, Term.Var(name)))
-          }
-          
-          // Type check
-          val ctx = TCtx(Env.empty, Map.empty, Nil)
-          val loc = Loc(file.getName, 1, 1)
-          try {
-            ctx.withDecls(loc, mutualDecls)
-            succeed
-          } catch {
-            case e: Exception => fail(s"Type check error: ${e.getMessage}")
-          }
+class TypeCheckSpec extends AnyFlatSpec with ParallelTestExecution {
+
+  val exampleFiles: Array[File] = Option(new File("examples").listFiles())
+    .getOrElse(Array.empty[File])
+    .filter(f => f.isFile && f.getName.endsWith(".ctt"))
+    .sortBy(_.getName)
+
+  exampleFiles.foreach { file =>
+    "Type checker" should s"successfully check ${file.getName}" in {
+      println(s"Testing ${file.getName}...")
+      val (_, _, modules) = TypeCheckSpec.loadModules(file.getPath)
+      val allDecls = modules.flatMap(_.declarations)
+      val (maybeErr, _) = TypeChecker.runDeclss(TEnv.silentEnv, allDecls)
+
+      maybeErr match {
+        case Some(err) => fail(s"Type check error: $err")
+        case None => succeed
       }
+    }
+  }
+}
+
+object TypeCheckSpec {
+  case class LoadedModule(
+    name: String,
+    declarations: List[Declarations],
+    names: List[(Ident, SymKind)]
+  )
+
+  def readFile(file: File): String =
+    Using.resource(Source.fromFile(file))(_.mkString)
+
+  def loadModules(
+    filePath: String,
+    notOk: Set[String] = Set.empty,
+    loaded: Set[String] = Set.empty,
+    modules: List[LoadedModule] = Nil
+  ): (Set[String], Set[String], List[LoadedModule]) = {
+    if (notOk.contains(filePath)) {
+      throw new RuntimeException(s"Looping imports in $filePath")
+    }
+    if (loaded.contains(filePath)) {
+      return (notOk, loaded, modules)
+    }
+
+    val file = new File(filePath)
+    if (!file.exists()) {
+      throw new RuntimeException(s"$filePath does not exist")
+    }
+
+    val source = readFile(file)
+    val expectedName = file.getName.stripSuffix(".ctt")
+
+    val parsedImports = Parser.parseImports(source) match {
+      case Left(err) => throw new RuntimeException(s"Parse failed in $filePath\n$err")
+      case Right(pi) =>
+        if (pi.name != expectedName) {
+          throw new RuntimeException(
+            s"Module name mismatch in $filePath: expected $expectedName, got ${pi.name}"
+          )
+        }
+        pi
+    }
+
+    val parent = Option(file.getParentFile).getOrElse(new File("."))
+    val importPaths = parsedImports.imports.map(imp => new File(parent, s"$imp.ctt").getPath)
+
+    var currentNotOk = notOk + filePath
+    var currentLoaded = loaded
+    var currentModules = modules
+
+    for (imp <- importPaths) {
+      val (no, lo, mo) = loadModules(imp, currentNotOk, currentLoaded, currentModules)
+      currentNotOk = no
+      currentLoaded = lo
+      currentModules = mo
+    }
+
+    val importedNames = currentModules.flatMap(_.names)
+
+    Parser.parseModule(source, expectedName, importedNames) match {
+      case Left(err) => throw new RuntimeException(s"Resolve failed in $filePath\n$err")
+      case Right(parsed) => (
+        notOk,
+        currentLoaded + filePath,
+        currentModules :+ LoadedModule(parsed.name, parsed.declarations, parsed.names)
+      )
     }
   }
 }
